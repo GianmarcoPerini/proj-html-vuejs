47cca3130309dc21d92f4d7046de1457c41db377
GianmarcoPerini <g.marco.perini@gmail.com>
2021-10-08 15:51:25 +0200

    refactor_3

--- src/components/Main/sectionSeven.vue
+++ src/components/Main/sectionSeven.vue
@@ -2,23 +2,26 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-6 text-center">
+                <div class="col-lg-6 col-12 text-center">
                     <div class="box black">
                         <p>Are You Ready?</p>
                         <h1>Start a New Project</h1>
                         <input class="w-75 mt-5" type="text" placeholder="Enter your email address">
                     </div>
                 </div>
-                <div class="col-6 text-center">
+                <div class="col-lg-6 col-12 text-center mt-lg-0 mt-4">
                     <div class="box green">
                         <p>What are you waiting for?</p>
                         <h1>Let's Talk About Work</h1>
                         <Button class="mt-5" :btnElement="msg" />
                     </div>
                 </div>
-                <div class="col-2 d-flex align-items-center" v-for="el in sponsor" :key="el.id">
-                    <Sponsor :path="el.path"/>
+                <div class="overflow-scroll">
+                    <div class="col-2 d-flex align-items-center" >
+                    <Sponsor class="p-4" :path="el.path" v-for="el in sponsor" :key="el.id"/>
                 </div>
+                </div>
+                
             </div>
         </div>
     </section>
@@ -57,7 +60,7 @@ export default {
 
 section{
     background-color: $bgPrimary;
-    padding: 100px;
+    padding: 100px 0;
 }
 
 .box{
